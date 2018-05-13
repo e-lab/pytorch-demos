93e8483e2dcb03c16d4edd51a8122056c530cf7c
Eugenio <culurciello@gmail.com>
2018-05-13 15:26:35 -0400

    added demo sine wave sequence rec

--- seq-learning-char/model.py
+++ seq-learning-char/model.py
@@ -17,7 +17,7 @@ class RNN(nn.Module):
         self.decoder = nn.Linear(hidden_size, output_size)
 
     def forward(self, input, hidden):
-        # print('in', input)
+        print('in', input)
         input = self.encoder(input.view(1, -1))
         # print('iin', input)
         output, hidden = self.gru(input.view(1, 1, -1), hidden)
@@ -31,26 +31,28 @@ class RNN(nn.Module):
 
 
 class CNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+    def __init__(self, input_size, hidden_size, output_size):
         super(CNN, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.n_layers = n_layers
+
+        print('SIZES: ', input_size, hidden_size, output_size)
 
         self.encoder = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
-        self.decoder = nn.Linear(hidden_size, output_size)
+        self.c1 = nn.Conv2d(1, hidden_size, 9, stride=4)
+        self.c2 = nn.Conv2d(hidden_size, hidden_size*2, 9, stride=4)
+        self.decoder = nn.Linear(hidden_size*4, output_size)
 
-    def forward(self, input, hidden):
-        # print('in', input)
+    def forward(self, input):
+        # print('in', input)]
         input = self.encoder(input.view(1, -1))
-        # print('iin', input)
-        output, hidden = self.gru(input.view(1, 1, -1), hidden)
-        output = self.decoder(output.view(1, -1))
+        print('iin', input.shape)
+        output = self.c1(input.view(1, 1, 128, 64))
+        print('out', output.shape)
+        # output = self.decoder(output.view(1, -1))
         # print('out', output)
-        return output, hidden
+        return output
 
     def init_hidden(self):
-        return Variable(torch.zeros(self.n_layers, 1, self.hidden_size))
-
+        return 0
--- seq-learning/train.py
+++ seq-learning/train.py
@@ -1,93 +1,264 @@
-# modified from: https://github.com/spro/practical-pytorch
+# E. Culurciello, May 2018
+# testing learning in LSTM, CNN, Attention neural networks: learning of sine-wave data
+# refer to: https://towardsdatascience.com/memory-attention-sequences-37456d271992
+# and: https://towardsdatascience.com/the-fall-of-rnn-lstm-2d1594c74ce0
+# inspired by https://github.com/pytorch/examples/blob/master/time_sequence_prediction/train.py
 
-import torch
-import numpy
-import torch.nn as nn
-from torch.autograd import Variable
 import argparse
 import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from profile import *
 
-from helpers import *
-from model import *
-from generate import *
 
-# Parse command line arguments
-argparser = argparse.ArgumentParser()
-# argparser.add_argument('filename', type=str)
-argparser.add_argument('--n_epochs', type=int, default=1000)
-argparser.add_argument('--print_every', type=int, default=25)
-argparser.add_argument('--hidden_size', type=int, default=50)
-argparser.add_argument('--n_layers', type=int, default=2)
-argparser.add_argument('--learning_rate', type=float, default=0.01)
-argparser.add_argument('--chunk_len', type=int, default=200)
-argparser.add_argument('--nseq', type=int, default=128)
-args = argparser.parse_args()
+parser = argparse.ArgumentParser(description='Learning sequences with LSTM, CNN, Attention')
+parser.add_argument('--sequencer', type=str, default='LSTM', 
+                    help='sequencer model to use: LSTM, CNN, Att')
+parser.add_argument('--epochs', type=int, default=5, 
+                    help='number of epochs to run')
+parser.add_argument('--pprint', type=bool, default=True, 
+                    help='Print PDF or display image only')
+args = parser.parse_args()
 
-import os
-filename='input.txt'
-if not os.path.isfile(filename):
-    os.system('wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt')
-file, file_len = read_file('input.txt')
+# generate sine wave data: batch of N with length L points
+def gen_data(T=20, L=1000, N=100):
+    x = np.empty((N, L))
+    x[:] = np.array(range(L)) + np.random.randint(-4 * T, 4 * T, N).reshape(N, 1)
+    data = np.sin(x / 1.0 / T)
+    return data
 
-def random_training_set(chunk_len):
-    start_index = random.randint(0, file_len - chunk_len)
-    end_index = start_index + chunk_len + 1
-    chunk = file[start_index:end_index]
-    inp = char_tensor(chunk[:-1])
-    target = char_tensor(chunk[1:])
-    return inp, target
+# profiling hooks:
+def add_hooks(m):
+    if len(list(m.children())) > 0: return
+    m.register_buffer('total_ops', torch.zeros(1))
+    m.register_buffer('total_params', torch.zeros(1))
 
-model = RNN(n_characters, args.hidden_size, n_characters, args.n_layers)
-model_optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
-criterion = nn.CrossEntropyLoss()
+    for p in m.parameters():
+        m.total_params += torch.Tensor([p.numel()])
 
-start = time.time()
-all_losses = []
-loss_avg = 0
+    if isinstance(m, nn.Conv2d):
+        m.register_forward_hook(count_conv2d)
+    if isinstance(m, nn.Conv1d):
+        m.register_forward_hook(count_conv1d)
+    elif isinstance(m, nn.BatchNorm2d):
+        m.register_forward_hook(count_bn2d)
+    elif isinstance(m, nn.ReLU):
+        m.register_forward_hook(count_relu)
+    elif isinstance(m, (nn.MaxPool1d, nn.MaxPool2d, nn.MaxPool3d)):
+        m.register_forward_hook(count_maxpool)
+    elif isinstance(m, (nn.AvgPool1d, nn.AvgPool2d, nn.AvgPool3d)):
+        m.register_forward_hook(count_avgpool)
+    elif isinstance(m, nn.Linear):
+        m.register_forward_hook(count_linear)
+    elif isinstance(m, nn.LSTMCell):
+        m.register_forward_hook(count_lstmcell)
+    elif isinstance(m, (nn.Dropout, nn.Dropout2d, nn.Dropout3d)):
+        pass
+    else:
+        print("Not implemented for ", m)
 
-in_array = numpy.zeros(args.nseq) # where we save inputs for conv(CNN)/attention models
-def shift(l, n):
-    return l[n:] + l[:n]
 
-def train(inp, target):
-    hidden = model.init_hidden()
-    model.zero_grad()
-    loss = 0
+class LSTM(nn.Module):
+    def __init__(self):
+        super(LSTM, self).__init__()
+        self.i1 = 51
+        self.lstm1 = nn.LSTMCell(1, self.i1)
+        self.lstm2 = nn.LSTMCell(self.i1, self.i1)
+        self.linear = nn.Linear(self.i1, 1)
+        print('LSTM model ops per sample:', 4*self.i1 + 4*self.i1^2 + self.i1)
 
-    for c in range(args.chunk_len):
-        in_array[0] = inp[c]
-        # print('be4', in_array) 
-        # shift(in_array, 1)
-        # print('after', in_array)
-        # break 
-        output, hidden = model(inp[c], hidden)
-        # print( 'batch:', inp[c].data.numpy(), numpy.argmax(output.data.numpy()), target[c].data.numpy())
-        loss += criterion(output, target[c])
+    def forward(self, input, future = 0):
+        outputs = []
+        h_t = torch.zeros(input.size(0), 51)
+        c_t = torch.zeros(input.size(0), 51)
+        h_t2 = torch.zeros(input.size(0), 51)
+        c_t2 = torch.zeros(input.size(0), 51)
 
-    loss.backward()
-    model_optimizer.step()
+        for j, input_t in enumerate(input.chunk(input.size(1), dim=1)): # 999 ti
+            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output] # append to list
 
-    return loss.data[0] / args.chunk_len
+        for j in range(future):# if we should predict the future
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2) # torch.Size([97, 1])
+            outputs += [output]
 
-def save():
-    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
-    torch.save(model, save_filename)
-    print('Saved as %s' % save_filename)
+        outputs = torch.stack(outputs, 1).squeeze(2)
+        return outputs # torch.Size([97, 999])
 
-try:
-    print("Training for %d epochs..." % args.n_epochs)
-    for epoch in range(1, args.n_epochs + 1):
-        loss = train(*random_training_set(args.chunk_len))
-        loss_avg += loss
+# 1-layer CNN:
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.L = 4 # how many past samples needed as inputs
+        self.doc1 = 8 # number of intermediate features
+        self.c1 = nn.Conv1d(1, self.doc1, self.L)
+        self.l1 = nn.Linear(self.doc1, 1)
+        print('CNN model ops per sample:', self.L*self.doc1+self.doc1)
 
-        if epoch % args.print_every == 0:
-            print('[elapsed time: %s, epoch: %d,  percent complete: %d%%, loss: %.4f]' % (time_since(start), epoch, epoch / args.n_epochs * 100, loss))
-            # print(generate(model, 'Wh', 100), '\n')
+    def forward(self, input, future = 0):
+        outputs = []
+        for i in range (0, self.L):
+            outputs += [torch.zeros(input.size(0), 1)] # fill with 0s
 
-    print("Saving...")
-    save()
+        for i in range(0, input.size(1)-self.L):
+            input_b = input[:,i:i+self.L].unsqueeze(1) # torch.Size([97, 1, 10])
+            o1 = self.c1(input_b)
+            o1= o1.view(input.size(0), self.doc1)
+            output = self.l1(o1)
+            outputs += [output]
 
-except KeyboardInterrupt:
-    print("Saving before quit...")
-    save()
+        for i in range(future):# if we should predict the future
+            output_b = outputs[input.size(1)-self.L-1+i:input.size(1)-1+i]
+            output_b = torch.stack(output_b, 1).squeeze().unsqueeze(1)
+            o1 = self.c1(output_b)
+            o1 = o1.view(input.size(0), self.doc1)
+            output = self.l1(o1)
+            outputs += [output]
 
+        outputs = torch.stack(outputs, 1).squeeze()
+        return outputs
+
+# Attention network:
+class Att(nn.Module):
+    def __init__(self):
+        super(Att, self).__init__()
+        self.L = 8 # how many attention features to use
+        self.a1 = Attention(self.L)
+        self.l1 = nn.Linear(self.L, 1)
+        print('Attention model ops per sample:', 2*self.L+3*self.L+self.L^2+self.L) # 2x mm, softmax, linear, linear l1
+
+    def forward(self, input, future = 0):
+        outputs = []
+        for i in range (0, self.L):
+            outputs += [torch.zeros(input.size(0), 1)] # fill with 0s
+            o1 = torch.zeros(input.size(0), 1, self.L)
+
+        for i in range(0, input.size(1)-self.L):
+            context = input[:,i:i+self.L].unsqueeze(1)
+            o1, attn = self.a1(o1, context)
+            o1 = F.relu(o1)
+            output = self.l1(o1)
+            outputs += [output.squeeze(1)]
+
+        for i in range(future):# if we should predict the future
+            context = outputs[input.size(1)-self.L-1+i:input.size(1)-1+i]
+            context = torch.stack(context, 1).squeeze().unsqueeze(1)
+            o1, attn = self.a1(o1, context)
+            o1 = F.relu(o1)
+            output = self.l1(o1)
+            outputs += [output.squeeze(1)]
+
+        outputs = torch.stack(outputs, 1).squeeze()
+        return outputs
+
+
+
+
+# set random seed to 0
+np.random.seed(0)
+torch.manual_seed(0)
+
+# load data and make training set
+data = gen_data()
+# train data:
+input = torch.from_numpy(data[3:, :-1]).float()
+target = torch.from_numpy(data[3:, 1:]).float()
+#test data:
+test_input = torch.from_numpy(data[:3, :-1]).float()
+test_target = torch.from_numpy(data[:3, 1:]).float()
+
+# print(input, target)
+# plt.plot(np.arange(input.size(1)), input[1,:input.size(1)].numpy(), linewidth = 2.0)
+# plt.show()
+
+# build the model:
+criterion = nn.MSELoss()
+if args.sequencer == 'LSTM':
+    model = LSTM()
+    model.apply(add_hooks)
+elif args.sequencer == 'CNN':
+    model = CNN()
+    model.apply(add_hooks)
+elif args.sequencer == 'Att':
+    from attention import Attention
+    model = Att()
+    model.apply(add_hooks)
+else:
+    print('No model specified!')
+    os._exit()
+    
+# use LBFGS as optimizer since we can load the whole data to train
+optimizer = optim.LBFGS(model.parameters(), lr=0.8)
+
+# begin to train
+for i in range(args.epochs):
+    print('STEP: ', i)
+    def closure():
+        optimizer.zero_grad()
+        out = model(input)
+        if args.sequencer == 'LSTM':
+            loss = criterion(out, target)
+        elif args.sequencer == 'CNN':
+            loss = criterion(out[:,model.L:], target[:,model.L:])
+        elif args.sequencer == 'Att':
+            loss = criterion(out[:,model.L:], target[:,model.L:])
+        print('Train loss:', loss.item())
+        loss.backward()
+        return loss
+    optimizer.step(closure)
+    
+    # begin to predict, no need to track gradient here
+    with torch.no_grad():
+        future = 1000
+        pred = model(test_input, future=future)
+        loss = criterion(pred[:, :-future], test_target)
+        print('TEST loss:', loss.item())
+        y = pred.detach().numpy()
+
+    # if loss < 0.003:
+        # break
+
+print('Training finished!\n')
+
+# draw the final result
+plt.figure(figsize=(30,10))
+plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
+plt.xlabel('x', fontsize=20)
+plt.ylabel('y', fontsize=20)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.ylim(-2, 2)
+def draw(yi, color):
+    plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
+    plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
+draw(y[0], 'r')
+draw(y[1], 'g')
+draw(y[2], 'b')
+if args.pprint:
+    plt.savefig('pred_%s.pdf'%args.sequencer)
+if not args.pprint:
+    plt.show()
+plt.close()
+
+# profile networks and print:
+total_ops = 0
+total_params = 0
+for m in model.modules():
+    if len(list(m.children())) > 0: continue
+    total_ops += m.total_ops
+    total_params += m.total_params
+total_ops = total_ops
+total_params = total_params
+
+print('Profiler results:')
+print("#Ops: %f GOps"%(total_ops/1e9))
+print("#Parameters: %f M"%(total_params/1e6))
